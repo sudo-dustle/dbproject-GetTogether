d29ee35271381e5cb2455bbf4ff3aafd3da5e940
Jisu <hellojs242@gmail.com>
2021-12-11 15:10:38 +0900

    refactor: 메세지 리스트 보기 로그인 방식 적용
    - 로그아웃 상태면 로그인페이지 redirection
    - 로그인한 사용자 memberNum 가져와서 반영

--- GetTogether/src/main/java/controller/message/ListMessageController.java
+++ GetTogether/src/main/java/controller/message/ListMessageController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import controller.member.MemberSessionUtils;
 import model.Message;
 import model.service.MessageManager;
 
@@ -14,13 +15,12 @@ public class ListMessageController implements Controller{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		// 우리는 Member지만 임시로 해놨습니다..
-//    	if (!UserSessionUtils.hasLogined(request.getSession())) {
-//            return "redirect:/user/login/form";		// login form 요청으로 redirect
-//        }
-    	//mnum을 가져오는 함수가 필요하다..
-    	int mnum = 2;
-    	//UserSessionUtils.getLoginUserNumber(request.getSession());
+
+    	if (MemberSessionUtils.hasLogined(request.getSession())) {
+            return "redirect:/user/login/form";		// login form 요청으로 redirect
+        }
+    	//mnum을 가져오는 함수
+    	int mnum = MemberSessionUtils.getLoginMemberNum(request.getSession());
     	MessageManager msgManager = MessageManager.getInstance();
     	List<Message> messageList = msgManager.findReceivedMessageList(mnum);
     	request.setAttribute("messageList", messageList);
